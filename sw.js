e21661de4728f12946c3611453fed153ba78d70d
Cedeno Hernandez <cedeno.hernandez@gmail.com>
2019-08-12 21:09:19 -0700

    fix: Service worker fix
    Removed the skipWaiting() hack that began to show errors and bugs in
    code.

--- sw.js
+++ sw.js
@@ -24,8 +24,6 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll(cacheFiles);
-    }).then(function() {
-      return self.skipWaiting();
     })
   );
 });
